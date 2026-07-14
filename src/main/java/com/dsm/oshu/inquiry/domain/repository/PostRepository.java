b95bed75aebba676111fb33b535abc5a3ee76521
Kanghee <kh63167574@gmail.com>
2026-07-14 14:31:59 +0900

    refactor : 파일명 구조 수정

--- src/main/java/com/dsm/oshu/inquiry/domain/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dsm.oshu.inquiry.domain.repository;
-
-import com.dsm.oshu.inquiry.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
